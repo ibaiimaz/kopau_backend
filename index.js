507bb1c033b8a305dfa137a5028d4f9489f0317a
ibaiimaz <ibaiimaz@gmail.com>
2017-09-12 21:49:54 +0200

    Testing deploy from GitHub

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ by: "pelayo" });
+  res.send({ by: "bai" });
 });
 
 const PORT = process.env.PORT || 5000;
