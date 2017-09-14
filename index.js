334b1124336d80a4f33e50fd3d78cf9492033522
ibaiimaz <ibaiimaz@gmail.com>
2017-09-14 23:16:16 +0200

    Add Google OAuth. Refacto. Add MongoDB and Mongoose

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ by: "bai" });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
