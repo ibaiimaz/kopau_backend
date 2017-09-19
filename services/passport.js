98dc27ead99a037e668e6ba81b03358ef0f962d0
ibaiimaz <ibaiimaz@gmail.com>
2017-09-19 17:30:31 +0200

    Add auth logic with cookie based authentication

--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,38 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accesToken, refreshToken, profile, done) => {
-        console.log('access token:', accesToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile', profile);
-    })
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accesToken, refreshToken, profile, done) => {
+            User.findOne({ googleID: profile.id }).then(existingUser => {
+                if (existingUser) {
+                    done(null, existingUser);
+                } else {
+                    new User({ googleID: profile.id }).save()
+                        .then(user => {
+                            done(null, user);
+                        });
+                }
+            });
+        }
+    )
 );
